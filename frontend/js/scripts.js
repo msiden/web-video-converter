1591956a56ee1016b7db1497747b1e4517331fe2
msiden <martin.siden@soprasteria.com>
2021-04-30 17:13:28 +0200

    upload function done

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,7 +1,8 @@
+const URL = "http://127.0.0.1:8000/"
 var request = new XMLHttpRequest()
 
 function Add () {
-    request.open("POST", "http://127.0.0.1:8000/add", true)
+    request.open("POST", URL + "add", true)
     request.setRequestHeader("accept", "application/json")
     request.setRequestHeader("Content-Type", "application/json")
     var newFilesFromElement = document.getElementById("newFilesToQueue").files;
@@ -17,62 +18,24 @@ function Add () {
     request.send(json)
 }
 
-let fileHandle;
-const pickerOpts = {
-    types: [
-        {
-            description: 'Videos'
-        },
-    ],
-    excludeAcceptAllOption: true,
-    multiple: true
-    };
-
-function upload() {
-    let videos = document.getElementById("filesToUpload").files[0];
+function Upload() {
     let formData = new FormData();
-    formData.append("files", videos);
-
-    //console.log(videos)
-    //console.log(formData);
-
-    //var file;
-    //var newFiles = [];
-    //for (var i = 0; i < videos.length; i++) {
-    //    file = videos[i];
-    //    newFiles.push(file);
-    //    console.log(file)
-    //}
-    //console.log(newFiles)
-    //formData.append("files", newFiles);
-    //formData.append("files", newFiles);
-
-    request.open("POST", "http://127.0.0.1:8000/upload");
-    //request.setRequestHeader("accept", "application/json")
-    //request.setRequestHeader("Content-Type", "multipart/form-data")
-    //request.setRequestHeader("Content-Type", "multipart/undefined")
+    var no_of_files = document.getElementById('filesToUpload').files.length;
+    for (var i = 0; i < no_of_files; i++) {
+        formData.append("files", document.getElementById('filesToUpload').files[i]);
+    }
+    request.open("POST", URL + "upload");
     request.send(formData);
-    //request.send(newFiles);
-    //request.send(videos);
+}
+
+function SetBitrate() {
 
 }
 
+function ProcessQueue() {
 
+}
 
+function DownloadFiles() {
 
-function Queue () {
-    request.open("GET", "http://127.0.0.1:8000/queue", true)
-    request.onload = function () {
-
-      var data = JSON.parse(this.response)
-
-      if (request.status >= 200 && request.status < 400) {
-        data.forEach((movie) => {
-          console.log(movie.title)
-        })
-      } else {
-        console.log('error')
-      }
-    }
-    request.send()
 }
