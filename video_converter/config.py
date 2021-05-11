ca30aa9babc103695b50d9960cfde130647af7c1
msiden <martin.siden@soprasteria.com>
2021-05-11 17:12:16 +0200

    adding Dockerfile

--- video_converter/config.py
+++ video_converter/config.py
@@ -1,6 +1,6 @@
 import socket
 import boto3
-import os
+
 
 ec2 = boto3.resource("ec2")
 ec2_instance = ec2.Instance("i-07b3a34e787e133d7")
@@ -12,5 +12,3 @@ LOCAL_IP = "127.0.0.1:8000"
 PUBLIC_IP = ec2_instance.public_ip_address
 LOCAL_URL = f"http://{LOCAL_IP}/"
 PUBLIC_URL = f"http://{PUBLIC_IP}/"
-
-os.environ["VIDEO_CONVERTER_IP"] = LOCAL_IP if DEV_MODE else PUBLIC_IP
